8c81bab1f4b63636107bb56ef4df42756b3f10d5
entbappy <entbappy73@gmail.com>
2023-07-12 10:28:54 +0600

    updated

--- app.py
+++ app.py
@@ -52,8 +52,6 @@ def index():
         return render_template('index.html')
 
 
-
-
 if __name__ == "__main__":
 	# app.run(host="0.0.0.0", port = 8080, debug=True)
 	app.run(host="0.0.0.0", port = 8080)
